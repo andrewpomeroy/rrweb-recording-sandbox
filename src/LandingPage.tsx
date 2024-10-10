add0cced06123121740561bfd6a289515699d8ce
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2024-10-10 16:48:31 -0700

    Initial commit

---
+++ src/LandingPage.tsx
@@ -0,0 +1,108 @@
+// LandingPage.tsx
+import React, { useState } from 'react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+interface Counter {
+  count: number;
+}
+
+const LandingPage = () => {
+  const [counter1, setCounter1] = useState<Counter>({ count: 0 });
+  const [counter2, setCounter2] = useState<Counter>({ count: 0 });
+
+  const handleIncrement = (counter: Counter) => {
+    switch (counter) {
+      case counter1:
+        setCounter1({ count: counter.count + 1 });
+        break;
+      case counter2:
+        setCounter2({ count: counter.count + 1 });
+        break;
+      default:
+        console.error('Unknown counter');
+    }
+  };
+
+  return (
+    <div className="min-h-screen w-full flex bg-gray-200">
+      <div className="max-w-[1280px] p-12 text-left">
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-3xl font-bold mb-2">Replay test page</h1>
+          </div>
+          <div className="flex space-x-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+
+        <p className="mb-16">This is a fake landing page.</p>
+        <div className="space-y-4">
+          <p>
+            Duis consectetur ea et cillum culpa nisi elit esse ipsum ea proident
+            laborum. Amet magna officia et pariatur est minim nostrud eu sunt
+            qui. Consectetur exercitation non est ipsum amet tempor. Nulla
+            aliquip reprehenderit ipsum sunt duis ea sunt.
+          </p>
+          <p>
+            Nostrud dolor enim dolor sint qui non irure. In pariatur mollit
+            minim nisi proident sint veniam et ad sit. Sunt sunt in ipsum
+            commodo quis excepteur consequat velit est. Fugiat occaecat do
+            aliquip elit quis do non cillum eu excepteur ex ullamco et aliqua
+            ullamco. Ipsum dolor dolor anim. Magna anim ad sint ex veniam esse
+            non laboris eu ut cupidatat nisi non velit.
+          </p>
+          <p>
+            Do laboris commodo ex consectetur ad dolore ipsum exercitation
+            aliquip. Adipisicing enim sint ex esse enim aliquip dolore. Deserunt
+            reprehenderit eiusmod dolore cillum consequat irure officia eiusmod
+            irure laborum. Commodo magna eu fugiat nulla. Cupidatat irure
+            officia aute et et commodo aute eiusmod amet est sint aliqua ut
+            aliquip. Consequat officia culpa velit ut id tempor enim laboris
+            cupidatat laborum laborum. In ad dolore officia irure occaecat
+            labore consequat veniam consectetur velit incididunt et. Fugiat est
+            ad Lorem dolor consequat qui culpa in minim voluptate exercitation
+            aliqua amet pariatur.
+          </p>
+          <div className="flex space-x-4">
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleIncrement(counter1)}
+            >
+              Counter (count: {counter1.count})
+            </button>
+            <button
+              className="bg-gray-500 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleIncrement(counter2)}
+              disabled
+            >
+              Disabled counter (count: {counter2.count})
+            </button>
+          </div>
+        </div>
+
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
